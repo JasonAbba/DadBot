57220d72326938657f02a93e2e4386bf7ae8ad59
Jason Abba <53237259+JasonAbba@users.noreply.github.com>
2024-12-10 21:53:34 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+const CONFIG = {
+  TOKEN_ENDPOINT_URL: "https://e72256a1349ee14b8f1a1b45fb93f0.d6.environment.api.powerplatform.com/powervirtualagents/botsbyschema/cre1d_dadBot/directline/token?api-version=2022-03-01-preview",
+};
+
+export{CONFIG};
